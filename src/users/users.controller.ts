7c620d5501dc05deb42c17a204480dbf9faf1213
Lazar Stojanović <lakisd98@gmail.com>
2026-05-25 11:10:22 +0200

    Add: get user & get all users controller route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,29 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAll(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
