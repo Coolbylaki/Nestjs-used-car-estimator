5b4316882ecff77f0a673782479f3de8614872b1
Lazar Stojanović <lakisd98@gmail.com>
2026-05-28 10:59:31 +0200

    Convert: built in nest serializer to custom one from scratch

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,11 +9,11 @@ import {
   Delete,
   NotFoundException,
   UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -24,7 +24,7 @@ export class UsersController {
     return this.usersService.find(email);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
